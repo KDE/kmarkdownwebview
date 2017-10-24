38373977da80d2f52c12ad50d65c33362698cb93
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-10-24 05:47:59 +0200

    Also extract strings from UI files

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc` >> rc.cpp
+$EXTRACTRC `find . -name \*.rc` `find . -name \*.ui` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp` -o $podir/kmarkdownwebview.pot
