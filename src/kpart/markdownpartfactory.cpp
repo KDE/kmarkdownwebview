960cf7ad200c7848a7eece7a198de7f53d293329
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-08-16 16:02:24 +0200

    Convert license headers to SPDX statements
    GIT_SILENT

--- src/kpart/markdownpartfactory.cpp
+++ src/kpart/markdownpartfactory.cpp
@@ -1,19 +1,8 @@
 /*
- * Copyright (C) 2017 by Friedrich W. H. Kossebau <kossebau@kde.org>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
+    SPDX-FileCopyrightText: 2017 Friedrich W. H. Kossebau <kossebau@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
 
 #include "markdownpartfactory.h"
 
@@ -30,7 +19,7 @@ MarkdownPartFactory::MarkdownPartFactory()
                   QStringLiteral(KMARKDOWNWEBVIEW_VERSION),
                   i18n("For the rendered display of Markdown files in a webpage-like layout"),
                   KAboutLicense::LGPL_V2_1,
-                  i18n("Copyright 2017,2020 Friedrich W. H. Kossebau"),
+                  i18n("SPDX-FileCopyrightText: 2017, 2020 Friedrich W. H. Kossebau "),
                   QString(),
                   QStringLiteral("https://commits.kde.org/kmarkdownwebview")
                  )
